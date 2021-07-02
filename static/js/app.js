97f7fb3cca9a71dc65b0e462b76dbdcdbaaaf4e5
Juanjo <juanjo_fdez@yahoo.com.mx>
2021-07-01 19:07:46 -0500

    Imports json file

---
+++ static/js/app.js
@@ -0,0 +1,19 @@
+// Use the D3 library to read in samples.json.
+
+// Reading the json
+d3.json("../data/samples.json").then((data) => {
+    console.log(data);
+})
+
+// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+
+
+
+
+// Use sample_values as the values for the bar chart.
+
+
+// Use otu_ids as the labels for the bar chart.
+
+
+// Use otu_labels as the hovertext for the chart.
